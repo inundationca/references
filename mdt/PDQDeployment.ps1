34921aa41ea8c89782ce42a33e2340f745218bb9
Tyler <tyler@inundation.ca>
2022-06-05 17:55:34 -0700

    script update

--- mdt/PDQDeployment.ps1
+++ mdt/PDQDeployment.ps1
@@ -1,6 +1,9 @@
 netsh advfirewall set allprofiles state off
 ipconfig /registerdns
 
+# Wait a few seconds to allow the DNS record to update.
+Start-Sleep 15
+
 Invoke-Command -ComputerName INU-S00-MDT01.ad.inundation.ca -ScriptBlock { ipconfig /flushdns; pdqdeploy.exe Deploy -Package "Deployment Package" -Targets $args[0] } -ArgumentList "$env:COMPUTERNAME"
 
 # Give PDQ Deploy a chance to start deploying to the device.
@@ -10,6 +13,3 @@ Start-Sleep 30
 while(test-path "C:\Windows\AdminArsenal\PDQDeployRunner\service-1.lock") {
 Start-Sleep 30
 }
-
-# Wait another 30 seconds to allow PDQ Inventory to scan the new device.
-Start-Sleep 30
